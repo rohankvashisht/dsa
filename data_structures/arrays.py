d51c1bc6a7adf0046b2a3b7ff8811205b949a530
r0hAn <13715543+rohankvashisht@users.noreply.github.com>
2022-03-02 13:00:30 +0530

    Create arrays.py

---
+++ data_structures/arrays.py
@@ -0,0 +1,23 @@
+# Array implementation in Python
+
+new_list = [5,8,3,7]
+result = new_list[0]
+
+if 1 in new_list: print(True)
+
+for n in new_list:
+  if n == 1:
+    print(True)
+
+    break
+
+# Adding to a list (3 methods)
+
+new_list.insert(0, 45)
+
+new_list.append(4)
+new_list.append([65,13,17])
+
+new_list.extend([6,9,4,8])
+
+print(new_list)
