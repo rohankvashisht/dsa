d930621510ad123755a6fd0f74810bd9aa08f935
r0hAn <13715543+rohankvashisht@users.noreply.github.com>
2022-03-07 13:02:03 +0530

    Update linked_list.py

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -116,6 +116,19 @@ class LinkedList:
         current = current.next_node
 
     return current
+
+  def node_at_index(self, index):
+    if index == 0:
+      return self.head
+    else:
+      current = self.head
+      position = 0
+
+      while position < index:
+        current = current.next_node
+        position += 1
+
+      return current
   
   def __repr__(self):
     """
