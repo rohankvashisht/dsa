8e9039fce0e3aa675887085f593796edbecb03d1
r0hAn <13715543+rohankvashisht@users.noreply.github.com>
2022-03-12 10:06:11 +0530

    Update load.py

--- algorithms/teamtreehouse/load.py
+++ algorithms/teamtreehouse/load.py
@@ -1,10 +1,6 @@
 # Video link : https://www.youtube.com/watch?v=8hly31xKli0
 
 """
-For random numbers: https://numbergenerator.org/numberlistrandomize
-"""
-
-"""
 You call this function with the path to a file you want to load. It loads the file contents, converts each line from a string to an integer, and returns them all as a Python list.
 """
 def load_numbers(file_name):
@@ -13,3 +9,10 @@ def load_numbers(file_name):
     for line in f:
       numbers.append(int(line))
   return numbers
+
+def load_strings(file_name):
+  strings = []
+  with open(file_name) as f:
+    for line in f:
+      strings.append(line.rstrip())
+  return strings
